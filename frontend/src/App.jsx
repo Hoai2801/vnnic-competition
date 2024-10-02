af810f160dc248192c1f28a2078344f44c9f21ca
AlexNe <truonggiang190689@gmail.com>
2024-10-02 18:12:13 +0700

    fix: layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import { lazy, Suspense, useEffect } from "react";
 import { Route, Routes, useLocation } from "react-router-dom";
 import Layout from "./components/Layout";
-import Topbar from "./components/Topbar";
 
 const Home = lazy(() => import("./pages/Home"));
 const Notfound = lazy(() => import("./pages/Notfound"));
@@ -18,18 +17,13 @@ export default function App() {
 
   return (
     <>
-      <Topbar />
-      <div className="flex min-h-screen w-full justify-center">
-        <div className="w-full max-w-[1170px]">
-          <Suspense fallback={<div>Loading...</div>}>
-            <Routes location={location} key={location.pathname}>
-              <Route index element={<Layout>{<Home />}</Layout>} />
-              <Route path="/login" element={<Layout>{<Login />}</Layout>} />
-              <Route path="*" element={<Notfound />} />
-            </Routes>
-          </Suspense>
-        </div>
-      </div>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes location={location} key={location.pathname}>
+          <Route index element={<Layout>{<Home />}</Layout>} />
+          <Route path="/login" element={<Layout>{<Login />}</Layout>} />
+          <Route path="*" element={<Notfound />} />
+        </Routes>
+      </Suspense>
     </>
   );
 }
--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
+import Topbar from "./Topbar";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="bg-secondary flex min-h-screen w-full justify-center">
       <div className="w-full max-w-[1170px] bg-white">
+        <Topbar />
         <Navbar />
         {children}
         <Footer />
