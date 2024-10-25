da7744977c219ba5a92efc1d22553cadcabbc25e
AlexNe <truonggiang190689@gmail.com>
2024-10-25 17:27:03 +0700

    - add robot plugins

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import { VitePluginRobots } from 'vite-plugin-robots'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePluginRobots({
+      userAgent: '*',
+      allow: '/',
+      disallow: '/private',
+      sitemap: 'https://tuoitregiadinh.org.vn/',
+    }),
+  ],
 })
